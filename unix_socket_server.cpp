b85a122fbc762f43a8184e50eaa451f2abdc49fd
amittrivedi565 <surfog00@gmail.com>
2025-06-17 13:05:45 +0530

    First Commit

--- unix_socket_server.cpp
+++ unix_socket_server.cpp
@@ -10,7 +10,7 @@ int main(){
     
     /*
      struct sockaddr_un{
-        sa_family_t sun_family    // Address family
+        sa_family_t sun_family    // Address family -> AF_UNIX declared below
         char        sun_path[108] // Path to the socket file
      }
      */
@@ -19,6 +19,8 @@ int main(){
     char buffer[100];
     
     // Create A Unix domain socket
+    // Returns a file descriptor
+    // Domain, Connection Oriented, Default Protocol
     sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
     
     if(sockfd == -1){
@@ -26,8 +28,10 @@ int main(){
         return 1;
     }
     
-    // Setting all values in addr to zero
+    // Setting all values in struct addr to zero
     memset(&addr, 0, sizeof(struct sockaddr_un));
+    
+    // sun_family type to AF_UNIX
     addr.sun_family = AF_UNIX;
     
     // Copies string "/tmp/my_unix_socket" to the addr.sun_path field
@@ -46,8 +50,41 @@ int main(){
         close(sockfd);
         return 1;
     }
-
     
+    std::cout << "Server listening on " << addr.sun_path << std::endl;
+    
+    client_fd = accept(sockfd, NULL, NULL);
+    if (client_fd == -1) {
+        std::cerr << "Accept failed!" << std::endl;
+        close(sockfd);
+        return 1;
+    }
+    
+    // Read data from the client
+    ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
+    if (bytes_read == -1) {
+        std::cerr << "Read failed!" << std::endl;
+        close(client_fd);
+        close(sockfd);
+        return 1;
+    }
+
+    buffer[bytes_read] = '\0';  // Null terminate the string
+    std::cout << "Received message: " << buffer << std::endl;
+
+       // Send a response to the client
+    const char* response = "Message received";
+    if (write(client_fd, response, strlen(response)) == -1) {
+        std::cerr << "Write failed!" << std::endl;
+        close(client_fd);
+        close(sockfd);
+        return 1;
+    }
+
+       // Clean up
+    close(client_fd);
+    close(sockfd);
+    unlink(addr.sun_path);
     
 }
     
